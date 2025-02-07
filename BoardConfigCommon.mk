5e239a4ad48d606870ed64678f13ff1bb314ffcc
Notkerd69 <29781314+Notkerd69@users.noreply.github.com>
2025-02-07 22:12:34 +0000

    sm6375-common: Switch dolby from hardware/motorola

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,12 +107,12 @@ endif
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/framework_manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/configs/vintf/device_framework_matrix.xml \
     vendor/yaap/config/device_framework_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
+DEVICE_MATRIX_FILE += $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/configs/vintf/manifest.xml
 
 # Metadata
@@ -203,3 +203,4 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # inherit from the proprietary version
 include vendor/motorola/sm6375-common/BoardConfigVendor.mk
+include hardware/motorola/dolby/BoardConfigVendor.mk
