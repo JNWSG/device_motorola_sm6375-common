c7ee99ec7e2fd45fb767198ddc7beb15eb1b21a7
TheImposter65 <tk5762996@gmail.com>
2025-04-09 17:17:02 +0530

    sm6375-common: Remove sec_config from tree * already shipped from vendor

--- common.mk
+++ common.mk
@@ -207,10 +207,6 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
-# IPC router config
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
-
 # Kernel
 OVERRIDE_ENABLE_UFFD_GC := false
 
