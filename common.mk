55b11a617db7b507962d0b6acf19db8b27acf860
Notkerd69 <29781314+Notkerd69@users.noreply.github.com>
2025-08-31 02:47:52 +0900

    sm6375-common: Use EROFS only for user builds
    Signed-off-by: Notkerd69 <29781314+Notkerd69@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -48,17 +48,31 @@ ifeq ($(TARGET_IS_VAB),true)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 endif
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
-    POSTINSTALL_OPTIONAL_system=true
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    AB_OTA_POSTINSTALL_CONFIG += \
+        RUN_POSTINSTALL_system=true \
+        POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+        FILESYSTEM_TYPE_system=erofs \
+        POSTINSTALL_OPTIONAL_system=true
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
-    POSTINSTALL_OPTIONAL_vendor=true
+    AB_OTA_POSTINSTALL_CONFIG += \
+        RUN_POSTINSTALL_vendor=true \
+        POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+        FILESYSTEM_TYPE_vendor=erofs \
+        POSTINSTALL_OPTIONAL_vendor=true
+else
+    AB_OTA_POSTINSTALL_CONFIG += \
+        RUN_POSTINSTALL_system=true \
+        POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+        FILESYSTEM_TYPE_system=ext4 \
+        POSTINSTALL_OPTIONAL_system=true
+
+    AB_OTA_POSTINSTALL_CONFIG += \
+        RUN_POSTINSTALL_vendor=true \
+        POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+        FILESYSTEM_TYPE_vendor=ext4 \
+        POSTINSTALL_OPTIONAL_vendor=true
+endif
 
 PRODUCT_PACKAGES += \
     checkpoint_gc \
