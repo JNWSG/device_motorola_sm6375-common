760d7a999e4ac0c9c38e5ed6f2636ea7006b0c85
TheImposter65 <tk5762996@gmail.com>
2025-11-15 18:12:26 +0530

    sm6375-common: ship init.qti.display_boot.rc

--- common.mk
+++ common.mk
@@ -155,6 +155,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     init.qti.display_boot.sh \
+    init.qti.display_boot.rc \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service
 
