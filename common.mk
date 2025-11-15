488168c5b2a9ec00f422d629ccbc1c8fd538329f
TheImposter65 <tk5762996@gmail.com>
2025-11-15 17:33:40 +0530

    Revert "sm6375-common: Implement OSS Dolby"
    This reverts commit 54ea0ca4863a07b56e8774e45826f92ae8aece72.

--- common.mk
+++ common.mk
@@ -17,16 +17,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-# Audio (Dolby Atmos)
-$(call inherit-product, hardware/motorola/dolby/setup.mk)
-
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Additional
 $(call inherit-product-if-exists, vendor/motorola/additional/additional-vendor.mk)
 
-# Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-yaap
 
