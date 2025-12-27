02d967b9479a4957579132db0d7ca1cd6b4b4ca2
BarryBlackCat <silva.tiago0910@gmail.com>
2025-12-27 13:10:49 +0000

    sm6375-common: Enable blurs
    Signed-off-by: BarryBlackCat <silva.tiago0910@gmail.com>

--- common.mk
+++ common.mk
@@ -131,6 +131,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Blurs
+TARGET_ENABLE_BLUR := true
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.1-impl \
